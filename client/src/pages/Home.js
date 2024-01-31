155933f72f31ebb27e8af894c9a51f6e3d727b5f
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2024-01-31 13:33:29 +0300

    a few changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import hondaImage from '../assets/jpg/honda.jpg';
 import camryImage from '../assets/jpg/camry.jpg';
 
@@ -7,29 +8,36 @@ function Home() {
   return (
     <div className='explore'>
     <header>
-          <p className='pageHeader'>MagariStela</p>
+      <div className='flex justify-start'>
+      <div class="flex flex-col items-center">
+                <p class="text-sm font-extrabold">Magari</p>
+                <p class="text-5xl italic text-green-400 font-extrabold">Stela</p>
+            </div>
+      </div>
+
+          
        
     </header>
     <main>
 
       <p className='exploreCategoryHeading'>What you looking for?</p>
       <div className='exploreCategories'>
-        <div>
+        <Link to="">
           <img 
             src={hondaImage}
             alt='honda accord'
             className='exploreCategoryImg'
           />
           <p className="exploreCategoryName">Honda Accord</p> 
-        </div>
-        <div>
+        </Link>
+        <Link to="">
           <img 
             src={camryImage}
             alt='toyota camry'
             className='exploreCategoryImg'
           />
           <p className="exploreCategoryName">Toyota Camry</p>
-        </div>
+        </Link>
       </div>
 
     </main>
